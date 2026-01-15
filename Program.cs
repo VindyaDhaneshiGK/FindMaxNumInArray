0e9d4ed03e4df623ece421a81223743035d67e52
Vindya Gardiyawasam <vindyadgardiyawasam@outlook.com>
2026-01-15 16:37:25 +0530

    change

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+ int FindMax(int[] nums)
+{
+    int max = 0;
+    if ( nums==null || nums.Length == 0)
+    {
+        throw new ArgumentException("Array is empty");
+    }
+    else
+    {
+        max= nums[0];
+        foreach (int num in nums)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+    }
+    return max;
+}
+int[] chkmax = { 2,3,5,10,-2,25};
+int result = FindMax(chkmax);
+Console.WriteLine("Maxcimum Number is:"+result);
+Console.ReadLine();
+
